baac9ac4187f5e8f8e03f21c6d134ec1c4ddb1dd
Andrea <86924796+andreac0@users.noreply.github.com>
2023-07-30 17:59:42 +0200

    add random degree

--- generate_data.py
+++ generate_data.py
@@ -12,8 +12,6 @@ from itertools import repeat
 number_patients = 10**6
 interval_start = '1/1/2021'
 interval_end = '31/12/2021'
-icu_percentage = 0.6
-death_percentage = 0.3
 variant_type = ['G6','K0','L8','M5','W3','Y6','J9']
 number_waves = 4
 ######################
@@ -35,9 +33,19 @@ regions = regions.iloc[
 regions = regions.reset_index(drop = True)
 regions.rename(columns = {'NUTS level 2':'Region','Country':'State'}, inplace = True)
 
-# Boolean for ICU admission
+# ID patients with covid
 N = list(range(1,number_patients+1))
-icuadmitted = random.choices([1, 0], weights=[icu_percentage, 1-icu_percentage], k=len(N))
+
+# Boolean for ICU admission
+icu_percentage = random.choices(range(1,10), k = 2)
+icuadmitted = random.choices([1, 0], weights=[icu_percentage[0], icu_percentage[1]], k=int(len(N)/(number_waves*4)))
+n = 20
+for i in range(1,number_waves*4):
+    new_icu = random.choices(range(1,n), k = 1)
+    icu_percentage = np.add(icu_percentage,np.array([new_icu[0],n-new_icu[0]]))
+    icuadmitted = np.append(icuadmitted,random.choices([1, 0], weights=[icu_percentage[0], icu_percentage[1]], k=int(len(N)/(number_waves*4))))
+    n = n*2
+
 
 # Dates for ICU admission
 d1 = datetime.strptime(interval_start, '%d/%m/%Y')
@@ -50,8 +58,13 @@ for i in range(len(N)):
 date_icu.sort()
 
 # Boolean for deceased
-deceased = random.choices([1, 0], weights=[death_percentage, 1-death_percentage], k=len(N))
+death_percentage = random.choices(range(1,10), k = 2)
+deceased = random.choices([1, 0], weights=[death_percentage[0], death_percentage[1]], k=int(len(N)/(number_waves*2)))
+for i in range(1,number_waves*2):
+    death_percentage = np.add(death_percentage,random.choices(range(1,10), k = 2))
+    deceased = np.append(deceased,random.choices([1, 0], weights=[death_percentage[0], death_percentage[1]], k=int(len(N)/(number_waves*2))))
 deceased = list(np.array(deceased) * np.array(icuadmitted))
+
 
 # Date deceased
 def dead(t):
@@ -68,10 +81,15 @@ for i in range(1,number_waves):
     variants = np.append(variants,random.choices(variant_type, weights=viral_intensity, k=int(len(N)/number_waves)))
 
 # Select region
-region_index = random.choices(range(1,len(regions)), k = len(N))
+weight_region = random.choices(range(1,1000), k = len(regions))
+region_index = random.choices(range(0,len(regions)), weights = weight_region, k=int(len(N)/number_waves))
+for i in range(1,number_waves):
+    weight_region = np.add(weight_region,random.choices(range(1,100), k = len(regions)))
+    region_index = np.append(region_index,random.choices(range(0,len(regions)), weights=weight_region, k=int(len(N)/number_waves)))
+
 patient_region = regions.iloc[region_index]
 
-
+# Union all data
 patient = pd.DataFrame({'id':N,'icuAdmitted':icuadmitted,
               'icuDate':date_icu,'deceased':deceased,
               'deathDate':date_dead, 'variant':variants, 
