906158756edbfb2834f45e6c323052a002706237
Andrea <86924796+andreac0@users.noreply.github.com>
2023-08-16 10:06:16 +0200

    improvements

--- generate_data.py
+++ generate_data.py
@@ -5,22 +5,28 @@ from random import randrange
 from datetime import datetime
 from datetime import timedelta
 from itertools import repeat
+import matplotlib.pyplot as plt 
 
 ######################
 ### Parameters
 #####################
 number_patients = 10**6
-interval_start = '1/1/2021'
-interval_end = '31/12/2021'
-variant_type = ['G6','K0','L8','M5','W3','Y6','J9']
+interval_start = '12/05/2021'
+interval_end = '30/12/2021'
+variant_type = ['G6','K0','L8','M5']#,'W3','Y6','J9']
 number_waves = 4
 ######################
+
+# Convert dates
+d1 = datetime.strptime(interval_start, '%d/%m/%Y')
+d2 = datetime.strptime(interval_end, '%d/%m/%Y')
 
 def random_date(start, end):
     delta = end - start
     int_delta = (delta.days)
     random_days = randrange(int_delta)
     return start + timedelta(days=random_days)
+
 
 # Regions
 regions = pd.read_excel("NUTS2021.xlsx", sheet_name='NUTS & SR 2021')
@@ -36,33 +42,44 @@ regions.rename(columns = {'NUTS level 2':'Region','Country':'State'}, inplace = 
 # ID patients with covid
 N = list(range(1,number_patients+1))
 
+# # Date for covid discovery
+# d1 = datetime.strptime(interval_start, '%d/%m/%Y')
+# d2 = datetime.strptime(interval_end, '%d/%m/%Y')
+
+# date_covid = []
+# for i in range(len(N)):
+#     date_covid.append(random_date(d1,d2))
+
+# date_covid.sort()
+
+
 # Boolean for ICU admission
 icu_percentage = random.choices(range(1,10), k = 2)
 icuadmitted = random.choices([1, 0], weights=[icu_percentage[0], icu_percentage[1]], k=int(len(N)/(number_waves*4)))
-n = 20
+n = 10
 for i in range(1,number_waves*4):
     new_icu = random.choices(range(1,n), k = 1)
     icu_percentage = np.add(icu_percentage,np.array([new_icu[0],n-new_icu[0]]))
-    icuadmitted = np.append(icuadmitted,random.choices([1, 0], weights=[icu_percentage[0], icu_percentage[1]], k=int(len(N)/(number_waves*4))))
+    w = icu_percentage/icu_percentage.sum()
+    icuadmitted = np.append(icuadmitted,random.choices([1, 0], weights=[w[0], w[1]], k=int(len(N)/(number_waves*4))))
     n = n*2
 
 
 # Dates for ICU admission
-d1 = datetime.strptime(interval_start, '%d/%m/%Y')
-d2 = datetime.strptime(interval_end, '%d/%m/%Y')
-
 date_icu = []
 for i in range(len(N)):
     date_icu.append(random_date(d1,d2))
 
 date_icu.sort()
 
+
 # Boolean for deceased
 death_percentage = random.choices(range(1,10), k = 2)
-deceased = random.choices([1, 0], weights=[death_percentage[0], death_percentage[1]], k=int(len(N)/(number_waves*2)))
-for i in range(1,number_waves*2):
+deceased = random.choices([1, 0], weights=[death_percentage[0], death_percentage[1]], k=int(len(N)/(number_waves*4)))
+for i in range(1,number_waves*4):
     death_percentage = np.add(death_percentage,random.choices(range(1,10), k = 2))
-    deceased = np.append(deceased,random.choices([1, 0], weights=[death_percentage[0], death_percentage[1]], k=int(len(N)/(number_waves*2))))
+    deceased = np.append(deceased,random.choices([1, 0], weights=[death_percentage[0], death_percentage[1]], k=int(len(N)/(number_waves*4))))
+
 deceased = list(np.array(deceased) * np.array(icuadmitted))
 
 
@@ -90,11 +107,12 @@ for i in range(1,number_waves):
 patient_region = regions.iloc[region_index]
 
 # Union all data
-patient = pd.DataFrame({'id':N,'icuAdmitted':icuadmitted,
+patient = pd.DataFrame({'id':N, 'icuAdmitted':icuadmitted,
               'icuDate':date_icu,'deceased':deceased,
               'deathDate':date_dead, 'variant':variants, 
               'Region':patient_region['Region'],
               'State':patient_region['State']}).reset_index(drop = True)
+
 
 # correct dates
 patient.loc[np.where(patient['deceased'] == 0)[0],'deathDate'] = None
@@ -102,5 +120,6 @@ patient.loc[np.where(patient['icuAdmitted'] == 0)[0],'icuDate'] = None
 patient.loc[np.where(patient['deathDate'] < patient['icuDate'])[0],['icuDate','deathDate']] = patient.iloc[np.where(patient['deathDate'] < patient['icuDate'])[0]][['deathDate','icuDate']].rename(columns = {'deathDate':'icuDate','icuDate':'deathDate'})
 
 patient.to_csv("patients.csv", index_label=None)
+
 
 
